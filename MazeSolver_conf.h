ba57387ab3cd1d3cff52de2120dab42da09219ae
dhruvagar69-ops <dhruvagar69@gmail.com>
2026-03-15 15:50:34 +0530

    MazeSolver_ conf added

---
+++ MazeSolver_conf.h
@@ -0,0 +1,24 @@
+#ifndef MAZESOLVER_CONF_H_
+#define MAZESOLVER_CONF_H_
+
+#define MAZE_SIZE 8
+
+#define MAZE_GOAL_LIST {IndexVec(1,0)}
+
+#define SEARCH_DEPTH1 1
+
+#define SEARCH_DEPTH2 20
+
+#define TURN90_TIME 	0.6
+
+#define TURN45_TIME 	0.4
+
+#define ACCELERATION 	6.0
+
+#define MAX_VELOCITY 	1.0
+
+#define MIN_VELOCITY 	0.2
+
+#define MAZE_1BLOCK_LENGTH 	0.18
+
+#endif
